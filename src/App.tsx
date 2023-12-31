6fea08a8ab8a6a334b7fac18bfb9a980b104b0be
aaaa1597 <tokushi2011@gmail.com>
2023-12-31 14:54:20 +0900

    最初のBOX表示

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, {useRef} from 'react';
 import './App.css';
 import { Canvas, useFrame, MeshProps  } from '@react-three/fiber'
 import * as THREE from 'three'
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, Environment } from '@react-three/drei'
 
 const Box = (props: MeshProps) => {
   const ref = useRef<THREE.Mesh>(null!)
@@ -16,7 +16,7 @@ const Box = (props: MeshProps) => {
   return (
     <mesh {...props} ref={ref}>
       <boxGeometry />
-      <meshBasicMaterial color={0x00ff00} wireframe />
+      <meshNormalMaterial />
     </mesh>
   )
 }
@@ -25,12 +25,11 @@ const App = () => {
   return (
     <div style={{ width: "75vw", height: "75vh" }}>
       <Canvas camera={{ position: [3, 1, 2] }}>
-        <ambientLight />
-        <directionalLight />
-        <Box position={[-0.75, 0, 0]} name="A" />
-        <Box position={[0.75, 0, 0]} name="B" />
+        <Box position={[1, 1, 1]} name="A" />
+        <Environment preset="forest" background />
         <OrbitControls />
-        <gridHelper rotation={[Math.PI / 4, 0, 0]} />
+        <axesHelper args={[5]} />
+        <gridHelper />
       </Canvas>
     </div>
   );
